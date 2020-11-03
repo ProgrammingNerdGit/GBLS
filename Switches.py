26a1e70629442a5a1636518a05c0dd2de2bebb6f
Programming Nerd <42655900+ProgrammingNerdGit@users.noreply.github.com>
2020-11-03 00:51:48 -0500

    Fixed Issue with 1 arg
    if you used 1 arg it would throw an error

--- Switches.py
+++ Switches.py
@@ -3,11 +3,13 @@ class switch:
         self.cases = []
         self.triggered = False
     def anyCase(self,func,*args):
+        if(len(args) <= 1): args += tuple([False])
         for i in args:
             if(args[i] and not self.triggered):
                 self.triggered = True
                 func()
     def exclusiveCase(self,func,*args):
+        if(len(args) <= 1): args += tuple([False])
         numOfExepts = 0
         for i in args:
             if(args[i] and not self.triggered):
@@ -19,4 +21,3 @@ class switch:
     def default(self,func):
         if(not self.triggered):
             func()
-    
